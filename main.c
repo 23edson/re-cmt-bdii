33ee858f78a14c539040fca88be2f576806731d5
LucasCP <ice0life@gmail.com>
2014-07-11 21:54:36 -0300

    criado na main o começo do create table

--- main.c
+++ main.c
@@ -3,10 +3,16 @@
 int main(int argc, char *argv[])
 {
 	buffer *bufferPool=NULL;
+	field *fieldAdd=NULL;
+	char *nome=NULL;
+	nome=(char*)malloc(sizeof(char)*TNAME_LENGHT);
+	if(!nome)
+		printf("\nSem Memoria\n");
+		
 	int choose = 0,i=1;
 	while(choose != 2)
 	{
-		printf("Escolha uma opção\n0- Preencher o Buffer\n1- Listar Buffer\n2- Sair\n");
+		printf("Escolha uma opção\n0- Preencher o Buffer\n1- Listar Buffer\n2- Sair\n4- criar tabela");
 		scanf("%d",&choose);
 		if(choose == 0)
 		{
@@ -43,6 +49,33 @@ int main(int argc, char *argv[])
 		}
 		else if(choose == 3){
 			extractTupleFromBP(bufferPool,i);
+		}
+		else if(choose == 4){
+			int nfields=0,i;
+			printf("\nDigite o nome da tabela: ");
+			fgets(nome,TNAME_LENGHT-1,stdin);
+			printf("\nQuantos Atributos terão: ");
+			scanf("%d",&nfields);
+			fieldAdd=(field*)malloc(sizeof(field)*nfields);
+			if(!fieldAdd){
+				printf("\nSem Memoria\n");
+				free(nome);
+				if(bufferPool){
+					free(bufferPool->fieldList);
+					free(bufferPool);
+				}
+				return 0;
+			}
+			for(i=0;i<nfields;i++){
+				printf("\nNome do Atributo: ");
+				fgets(fieldAdd[i].fName,TNAME_LENGHT-1,stdin);
+				printf("\nTipo do Atributo S, I, D ou C: ");
+				scanf("%c",&fieldAdd[i].fType);
+				printf("\nTamanho do Atributo (numero): ");
+				scanf("%d",&fieldAdd[i].fLenght);
+			};
+			
+			free(fieldAdd);
 		};
 	};
 	
@@ -50,6 +83,7 @@ int main(int argc, char *argv[])
 		free(bufferPool->fieldList);
 		free(bufferPool);
 	}
+	free(nome);
 	return 0;
 
 }
